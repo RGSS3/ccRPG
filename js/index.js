b13e1689e2364a28f9a464d18b758a7931803105
seiran <924172963@qq.com>
2017-10-15 03:55:11 +0800

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+require.config({
+    paths: {
+        'unpkg': 'https://unpkg.com',
+        'cc':    '/ccRPG'
+    }
+});
+
+document.body.insertAdjacentHTML('afterBegin', '<div id="app"></div>');
